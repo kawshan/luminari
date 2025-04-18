d1a82f9703089527118fcac0ea4f8bd53a767a69
kawshan <kawshan.virantha9999@gmail.com>
2025-04-18 22:34:08 +0530

    finished navbar

---
+++ src/app/welcome/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NavBar} from "@/app/welcome/NavBar";
+
+function Page() {
+    return (
+        <div>
+
+            <NavBar />
+
+
+        </div>
+    );
+}
+
+export default Page;
