27c56d99b13c025954e7a59843fa8a6d822cadf4
kawshan <kawshan.virantha9999@gmail.com>
2025-04-19 13:57:05 +0530

    main parts are done

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,43 +1,29 @@
-import React from 'react';
+"use client"
+import React, {useEffect} from 'react';
 import {NavBar} from "@/app/welcome/NavBar";
 import Image from "next/image";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import {mainText} from "@/lib/constance/norway"
+import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
+
 
 
 
 
 function Page() {
 
-    const norwayMainText = [
-        {
-            text: "KITCHEN",
-            className: "text-white",
-        },
-        {
-            text: "AT",
-            className: "text-white",
-        },
-        {
-            text: "THE",
-            className: "text-white",
-        },
-        {
-            text: "END",
-            className: "text-white",
-        },
-        {
-            text: "OF",
-            className: "text-white",
-        },
-        {
-            text: "THE",
-            className: "text-white",
-        },
-        {
-            text: "WORLD",
-            className: "text-white",
-        }
-    ];
+    useEffect(()=>{
+        getNorwayMainText()
+    },[])
+
+
+    function getNorwayMainText ():any{
+        return mainText.map(oneText=>({
+            text: oneText.text,
+            className: oneText.className,
+        }))
+    }
+
 
 
 
@@ -49,8 +35,15 @@ function Page() {
                 <Image src="/images/norway.jpg" alt="Norway" fill className="h-auto max-w-full" />
 
                 <div className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-white">
-                    <TypewriterEffectSmooth className="text-white" words={norwayMainText} />
+                    <TypewriterEffectSmooth className="text-white" words={getNorwayMainText()} />
                 </div>
+
+                <div className="absolute top-6/10 left-4/10 text-center text-xl text-white">
+                    <p className="text-white">
+                        Norway: Pushing the boundaries for foodie adventurers
+                    </p>
+                </div>
+
 
             </div>
 
---
+++ src/lib/constance/norway.tsx
@@ -0,0 +1,9 @@
+export const mainText: { text: string; className: string }[] = [
+    { text: "KITCHEN", className: "text-white" },
+    { text: "AT", className: "text-white" },
+    { text: "THE", className: "text-white" },
+    { text: "END", className: "text-white" },
+    { text: "OF", className: "text-white" },
+    { text: "THE", className: "text-white" },
+    { text: "WORLD", className: "text-white" }
+];
